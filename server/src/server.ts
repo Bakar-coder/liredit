fd7a6408a8b6c9319de82e98cd6f8babd08916ce
Bakar-coder <freelancebakar@gmail.com>
2021-06-02 18:26:07 +0300

    cleaned up

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,10 +14,10 @@ import { createConnection } from "typeorm";
 import { mkdir, readdir } from "fs";
 import {
   CORS_ORIGIN,
+  DB_HOST,
   DB_TYPE,
   PORT,
   PRIVATE_KEY,
-  REDIS_URL,
   __prod__,
 } from "./_constants";
 import { dbConfig } from "./db-config";
@@ -44,7 +44,11 @@ filePaths.map((filePath) =>
 
 const server = async () => {
   const app = express();
-  const redis = new Redis(REDIS_URL, { tls: {} });
+  const redis = new Redis({
+    port: 6379,
+    host: DB_HOST,
+    enableAutoPipelining: true,
+  });
   const redisStore = connectRedis(session);
   const conn = await createConnection({
     ...dbConfig,
