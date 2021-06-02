5eb01eb16cda11b9a40af645dd1accdf939a8250
Bakar-coder <freelancebakar@gmail.com>
2021-06-02 13:40:06 +0300

    cleaned up

--- server/src/server.ts
+++ server/src/server.ts
@@ -47,7 +47,7 @@ const server = async () => {
   const redis = new Redis({
     port: 6379,
     host: DB_HOST,
-    connectTimeout: 30000,
+    connectTimeout: 10000,
   });
   const redisStore = connectRedis(session);
   const conn = await createConnection({
