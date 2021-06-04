3d972fbc81dbc4e595bf79b82b7a67ffd7c5008d
Bakar-coder <freelancebakar@gmail.com>
2021-06-04 07:39:09 +0300

    cleanups

--- client/src/pages/shop/admin/update-product/[title].tsx
+++ client/src/pages/shop/admin/update-product/[title].tsx
@@ -1,12 +1,12 @@
 import { NextPage } from "next";
-import router from "next/router";
+import Router from "next/router";
 import { withUrqlClient } from "next-urql";
 import React from "react";
 import EditProduct from "../../../../components/admin/editProduct";
 import Layout from "../../../../components/layouts";
 import { createUrqlClient } from "../../../../utils/urqlClient";
 const UpdateProduct: NextPage = () => {
-  const title = router.query.title as string;
+  const title = Router.query.title as string;
   return (
     <Layout>
       <EditProduct productTitle={title} />
